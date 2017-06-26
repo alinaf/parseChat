ea7ee0aef9315f6ca6f7cc85c0fd6088adced16b
Claire Chen <cokeity@fb.com>
2017-06-26 14:01:59 -0700

    added alert controller

--- parseChat/ViewController.swift
+++ parseChat/ViewController.swift
@@ -12,24 +12,69 @@ import Parse
 class ViewController: UIViewController {
     
     //Outlets
-    
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     
-
-    @IBAction func clickedSignUp(_ sender: UIButton) {
-        let newUser = PFUser()
+    //Variables
+    
+    func showAlert(){
+        let alertController = UIAlertController(title: "Error", message: "Did not enter username or password correctly.", preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            //handle response here
+        }
+        alertController.addAction(OKAction)
+        present(alertController, animated: true){
+            //optional code for what happens after the alert controller has finished presenting
+        }
         
-        newUser.username = usernameTextField.text
-        //newUser.email = emailTextField.text
-        newUser.password = passwordTextField.text
+    }
+    
+    
+    @IBAction func clickedSignUp(_ sender: UIButton) {
+        
+        if (self.usernameTextField.text?.isEmpty == true || (self.passwordTextField.text?.isEmpty == true)){
+            showAlert()
+        } else {
+            let newUser = PFUser()
+            
+            newUser.username = usernameTextField.text
+            //newUser.email = emailTextField.text
+            newUser.password = passwordTextField.text
+            
+            newUser.signUpInBackground { (success: Bool, error: Error?) in
+                if let error = error {
+                    print(error.localizedDescription)
+                } else {
+                    print("Sign Up Successful!")
+                    
+                }
+            }
+        }
+        
         
         
         
     }
     
     
-    @IBAction func clickedLogin(_ sender: Any) {
+    @IBAction func clickedLogin(_ sender: UIButton) {
+        let username = usernameTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        if ((self.usernameTextField.text?.isEmpty == true) || self.passwordTextField.text?.isEmpty == true){
+            showAlert()
+        } else {
+            PFUser.logInWithUsername(inBackground: username, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("User Login Failed: \(error.localizedDescription)")
+            } else {
+                print("User Login Successful!")
+                }
+            }
+        }
+        
+        
+        
     }
     
     
